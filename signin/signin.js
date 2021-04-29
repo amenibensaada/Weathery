d4cdb1589a96663fa727fd585e18710d6aba7527
amenibensaada <amenisaada19@gmail.com>
2021-04-29 22:48:56 +0100

    fixing some issues

--- signin/signin.js
+++ signin/signin.js
@@ -1,11 +1,8 @@
 document.querySelector(".button").onclick = function () {
   var password = document.querySelector(".password").value;
-  var confirmpassword = document.querySelector(".confirmpassword").value;
 
   if (password == "") {
     alert("Field can not be empty ");
-  } else if (password !== confirmpassword) {
-    alert("Password didn't match try again");
   } else if (password !== "") {
     window.location.href = "../pages/choose.html";
   }
